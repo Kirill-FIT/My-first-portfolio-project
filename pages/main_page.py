54f1880afeda80d71f14d88ab9847c8c257ca324
Kirill-FIT <k_p_g88@mail.ru>
2020-03-22 21:01:07 +0500

    Added a transition between pages.

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage               # Переходы между страницами. Первый способ: возвращать нужный Page Object.
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
@@ -14,3 +15,15 @@ class MainPage(BasePage):
     """
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        
+    # Переходы между страницами:
+    """Первый способ: возвращать нужный Page Object.
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
+    """
+    # Второй подход: переход происходит неявно, страницу инициализируем в теле теста:
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
