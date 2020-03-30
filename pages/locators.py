c9f78d6bbe93384873e680d7e55bafcd04758243
Kirill-FIT <k_p_g88@mail.ru>
2020-03-30 14:51:46 +0500

    Added a class for the product page.

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_SUBMIT = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUTTON_ADD = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
