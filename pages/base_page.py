91ab9953a8fe06ed219d96b7eb61cb69a57ac83d
Kirill-FIT <k_p_g88@mail.ru>
2020-03-30 14:17:24 +0500

    Added an exception method.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException             #имя_исключения
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
-    """def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-    """
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
@@ -12,7 +8,7 @@ class BasePage():
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except NoSuchElementException:                                    #(имя исключения)
+        except NoSuchElementException:
             return False
         return True
     def open(self):
