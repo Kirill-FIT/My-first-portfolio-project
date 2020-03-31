e4349b7d42c9368b22bd8c3e282c5ec713cd5388
Kirill-FIT <k_p_g88@mail.ru>
2020-03-31 18:19:28 +0500

    Added new tests.

--- test_product_page.py
+++ test_product_page.py
@@ -10,17 +10,17 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-veb_sites = [
+"""veb_sites = [
     ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"),
-    """("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3"),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4"),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5"),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"),
-    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="fixing this bug right now")),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8"),
-    ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9")"""
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3"),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4"),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5"),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"),
+    # pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="fixing this bug right now")),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8"),
+    # ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9")
 ]
 
 @pytest.mark.parametrize('page', veb_sites)
@@ -60,4 +60,16 @@ class TestPage(object):
         page.should_be_button_add_product_to_basket()
         page.push_button_add_product_to_basket()
         page.should_be_quiz()
-        page.should_be_success_message_is_disappeared()
+        page.should_be_success_message_is_disappeared()"""
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+        
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
