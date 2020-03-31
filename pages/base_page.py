80534743d7f5e41d8f46c8922bf02ebc01979068
Kirill-FIT <k_p_g88@mail.ru>
2020-03-31 13:46:21 +0500

    Added a method for negative validation.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,16 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import math
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout) # закомментить необходимо, чтобы корректно отрабатывали индивидуальные методы WebDriverWait -> until и WebDriverWait -> until_not (см. ниже). т.е. тем самым отключить неявные ожидания
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -15,6 +19,21 @@ class BasePage():
         return True
     def open(self):
         self.browser.get(self.url)
+    
+    def is_not_element_present(self, how, what, timeout=4): # упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+    def is_disappeared(self, how, what, timeout=4): # будет ждать до тех пор, пока элемент не исчезнет
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+    
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
