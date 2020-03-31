3694650462a10462797f1a4cb78fe42f1617005a
Kirill-FIT <k_p_g88@mail.ru>
2020-03-31 13:53:12 +0500

    Added methods for negative checking items on a product page.

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,14 +11,24 @@ class ProductPage(BasePage):
         button_add_product_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD)
         button_add_product_to_basket.click()
     def should_be_quiz(self):
-        self.solve_quiz_and_get_code() # считывает x, выполняет расчет по формуле, вставляет значение, жмет ок 
+        self.solve_quiz_and_get_code() # считывает x, выполняет расчет по формуле, вставляет значение, жмет ок
+        
+    def should_not_be_success_message(self): # сообщение об успехе представлено, но не должно быть
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be."
+    def should_be_success_message_is_disappeared(self): # сообщение об успехе представлено, но должно исчезнуть
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should is disappeared."    
+        
     def should_be_name_equal(self):
+        """
         # проверяем появились ли элементы с сообщениями о добавлении продукта
         assert self.is_element_present(*ProductPageLocators.SUCCESS_MESAGE), ('Success message is not present')
+        """
         # записываем в переменную product_name текст из элемента product_main h1
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         # записываем в переменную in_message_name текст из элемента #messages
-        in_message_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESAGE).text
+        in_message_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
         # с помощью assert проверяем, что текст переменной product_name СОДЕРЖИТСЯ в тексте переменной с сообщением о добавлении продукта
         assert product_name == in_message_name, f'"{product_name}" is not "{in_message_name}"'
     def should_be_prise_equal(self):
