9c5e74f9ddec1820ebd9f8b4f53f7d5e8c27da40
Kirill-FIT <k_p_g88@mail.ru>
2020-03-31 18:05:39 +0500

    Added a new class BasePageLocators.

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,9 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
 class LoginPageLocators():
     LOGIN_SUBMIT = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "#register_form")
