dae6b93a4b68b91e7aa1ffc8e4f70ac7d26f3f08
Kirill-FIT <k_p_g88@mail.ru>
2020-04-03 19:09:53 +0500

    Combined tests for the user in one class. Added fixture setup to the class.

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
-from .pages.product_page import ProductPage
-from .pages.basket_page import BasketPage
+from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 import pytest
+import time
+
 
 @pytest.fixture(scope="function")
 def browser():
@@ -61,24 +64,79 @@ class TestPage(object):
         page.should_be_button_add_product_to_basket()
         page.push_button_add_product_to_basket()
         page.should_be_quiz()
-        page.should_be_success_message_is_disappeared()"""
+        page.should_be_success_message_is_disappeared()
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link_on_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = BasketPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        page.should_not_be_basket_items()
+        page.should_be_message_basket_is_empty()
+"""
+
+@pytest.mark.webtest # маркер webtest зарегистрировал в pytest.ini
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org" # генерация email c использованием текущего времени
+        password = str(time.time()) + "fakepa$$word" # генерация password c использованием текущего времени
+        login_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+        page = LoginPage(browser, login_link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
         
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_button_add_product_to_basket()
+        page.push_button_add_product_to_basket()
+        page.should_be_quiz()
+        page.should_be_name_equal()
+        page.should_be_prise_equal()
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.should_not_be_basket_items()
-    page.should_be_message_basket_is_empty()
+
+""" Создание объектов по API.
+Cоздание нового товара в нашем интернет-магазине перед тестом и удаление по завершении теста:
+@pytest.mark.login
+class TestLoginFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.product = ProductFactory(title = "Best book created by robot")
+        # создаем по апи
+        self.link = self.product.link
+        yield
+        # после этого ключевого слова начинается teardown
+        # выполнится после каждого теста в классе
+        # удаляем те данные, которые мы создали 
+        self.product.delete()
+        
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        page = ProductPage(browser, self.link)
+        # дальше обычная реализация теста
+
+    def test_guest_should_see_login_link(self, browser):
+        page = ProductPage(browser, self.link)
+        # дальше обычная реализация теста
+"""
