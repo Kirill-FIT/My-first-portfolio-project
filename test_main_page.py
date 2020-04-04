88899b8bb176da07ed543b3140d7eeaa695c1b68
Kirill-FIT <k_p_g88@mail.ru>
2020-04-04 21:00:51 +0500

    Checked and, if necessary, adjusted the code. (Final check of the project)

--- test_main_page.py
+++ test_main_page.py
@@ -13,7 +13,7 @@ class TestLoginFromMainPage():
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
-
+        
     def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
         page = BasketPage(browser, link)
