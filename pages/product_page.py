fdff7888d333ebbe9c06e9a08dec415eb76d9a85
Kirill-FIT <k_p_g88@mail.ru>
2020-04-04 20:44:30 +0500

    Checked and, if necessary, adjusted the code. (Final check of rhe project)

--- pages/product_page.py
+++ pages/product_page.py
@@ -11,7 +11,7 @@ class ProductPage(BasePage):
         button_add_product_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD)
         button_add_product_to_basket.click()
     def should_be_quiz(self):
-        self.solve_quiz_and_get_code() # считывает x, выполняет расчет по формуле, вставляет значение, жмет ок
+        self.solve_quiz_and_get_code()
         
     def should_not_be_success_message(self): # сообщение об успехе представлено, но не должно быть
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
@@ -21,15 +21,9 @@ class ProductPage(BasePage):
             "Success message is presented, but should is disappeared."    
         
     def should_be_name_equal(self):
-        """
-        # проверяем появились ли элементы с сообщениями о добавлении продукта
-        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESAGE), ('Success message is not present')
-        """
-        # записываем в переменную product_name текст из элемента product_main h1
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), ('Success message is not present')
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        # записываем в переменную in_message_name текст из элемента #messages
         in_message_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
-        # с помощью assert проверяем, что текст переменной product_name СОДЕРЖИТСЯ в тексте переменной с сообщением о добавлении продукта
         assert product_name == in_message_name, f'"{product_name}" is not "{in_message_name}"'
     def should_be_prise_equal(self):
         assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL), ('Total cost message is not ptesent')
